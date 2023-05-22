a5a40fd47240808b8ac4940cf820fc137224edcf
ralphbetta <ralphbetta@gmail.com>
2023-05-22 13:47:09 +0100

    implement sequelize cli migration to fix  unique: true in the product services and updated the product model

--- product-service/src/model/product.model.js
+++ product-service/src/model/product.model.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
     name: {
       type: DataTypes.STRING,
-      // allowNull: false,
+      allowNull: false,
+      unique: true,
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
